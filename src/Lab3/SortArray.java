8442a1f6041717606a68c01e43a62933f156da85
Nguyet Ha <ntnguyetha1998@gmail.com>
2022-09-21 21:14:15 +0700

    add lab

--- src/Lab3/SortArray.java
+++ src/Lab3/SortArray.java
@@ -1,16 +1,22 @@
 package Lab3;
 
+import java.util.Arrays;
+
 public class SortArray {
     public static void main(String[] args) {
-        int arr[] = new int[]{1, 2, 3, 22, 5, 17, 31, 26, 12, 55, 32};
-        for (int i = 0; i < arr.length-1; i++) {
-            int b;
-            if (arr[i]> arr[i+1]){
-                b= arr[i];
-                arr[i]=arr[i+1];
-                arr[i+1]=b;
+        int arr[] = new int[]{55, 10, 3, 22, 5, 17, 31, 26, 12, 55, 32};
+        int b;
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] > arr[j]) {
+                    b = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = b;
+                }
             }
-            System.out.println(arr[i]);
+        }
+        for (int k : arr) {
+            System.out.println(k);
         }
 
     }
--- src/Lab4/SimpleMenu.java
+++ src/Lab4/SimpleMenu.java
@@ -6,12 +6,13 @@ import java.util.Scanner;
 
 public class SimpleMenu {
     public static void printMenu() {
-        System.out.println("=======Menu======");
         System.out.println("1. Add number to array list");
         System.out.println("2. Print numbers");
         System.out.println("3. Get maximum number");
         System.out.println("4. Get minimum number");
         System.out.println("5. Search number");
+        System.out.println("6. Exit!");
+        System.out.println("Please choose menu: ");
     }
 
     public static int getNumberFromKeyboards() {
@@ -40,14 +41,22 @@ public class SimpleMenu {
         return min;
     }
 
+    public static boolean checkEmptyList(List<Integer> list) {
+        if (list.isEmpty()) {
+            return false;
+        }
+        return true;
+    }
+
     public static int searchNumber(List<Integer> list) {
-        int number = getNumberFromKeyboards();
         int result = -1;
+        int number = getNumberFromKeyboards();
         for (int i = 0; i < list.size(); i++) {
             if (list.get(i) == number) {
                 result = i;
                 break;
             }
+
         }
         return result;
     }
@@ -55,36 +64,54 @@ public class SimpleMenu {
 
     public static void main(String[] args) {
         List<Integer> listNumber = new ArrayList<>();
-        for (int i = 0; i < 15; i++) {
-            listNumber.add(i);
-        }
-        printMenu();
-        System.out.print("Pls choose menu: ");
-        Scanner scanner = new Scanner(System.in);
-        int option = scanner.nextInt();
+//        listNumber.add(2);
+//        listNumber.add(4);
+//        listNumber.add(2);
+//        listNumber.add(11);
+//        listNumber.add(9);
+//        listNumber.add(6);
+        boolean out = true;
+        while (out) {
+            System.out.println("\n ---------------Menu-------------");
+            printMenu();
+            Scanner scanner = new Scanner(System.in);
+            int option = scanner.nextInt();
 
-        switch (option) {
-            case 1:
-                int number = getNumberFromKeyboards();
-                listNumber.add(number);
-                System.out.println("Your array include :" + listNumber.toString());
-                break;
-            case 2:
-                int number2 = getNumberFromKeyboards();
-                System.out.println("Your number input is: " + number2);
-                break;
-            case 3:
-                System.out.println("Max number in array is: " + getMaxNumber(listNumber));
-                break;
-            case 4:
-                System.out.println("Min number in array is: " + getMinNumber(listNumber));
-                break;
-            case 5:
-                int result = searchNumber(listNumber);
-                if (result==-1){
-                    System.out.println("Your number can't find in array");
-                }
-                else System.out.println(" Your number in index: "+ result);
+            switch (option) {
+                case 1:
+                    int number = getNumberFromKeyboards();
+                    listNumber.add(number);
+                    System.out.println("Your array include :" + listNumber.toString());
+                    break;
+                case 2:
+                    int number2 = getNumberFromKeyboards();
+                    System.out.println("Your number input is: " + number2);
+                    break;
+                case 3:
+                    if (checkEmptyList(listNumber)) {
+                        System.out.println("Max number in array is: " + getMaxNumber(listNumber));
+                    } else System.out.println("The array is null");
+                    break;
+                case 4:
+                    if (checkEmptyList(listNumber)) {
+                        System.out.println("Min number in array is: " + getMinNumber(listNumber));
+                    } else System.out.println("The array is null");
+                    break;
+                case 5:
+                    if (checkEmptyList(listNumber)) {
+                        int result = searchNumber(listNumber);
+                        if (result == -1) {
+                            System.out.println("Your number can't find in array");
+                        } else System.out.println(" Your number in index: " + result);
+                    } else System.out.println("The array is null");
+                    break;
+                case 6:
+                    out = false;
+                    System.out.println("Exit!...");
+                    break;
+                default:
+                    System.out.println(" Pls choose your option in menu!");
+            }
         }
     }
 }
